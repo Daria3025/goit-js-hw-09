45bf55bdd5ea4012e7eb320bcbe59842e647e45d
Daria3025 <dtimofeeva191@icloud.com>
2023-08-02 20:41:53 +0300

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
+
+stopBtn.setAttribute('disabled', true);
+let timerID = null;
+
+function onStart() {
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+    timerID = setInterval(() => {
+        const bgColor = getRandomHexColor();
+        body.style.backgroundColor = bgColor;
+    }, 1000);
+}
+
+function onStop() {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+    clearInterval(timerID);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
