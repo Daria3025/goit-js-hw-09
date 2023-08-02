a5a8ebf42f115943422bc10153a40b3ce2838474
Daria3025 <125704493+Daria3025@users.noreply.github.com>
2023-08-02 17:42:16 +0000

    Deploying to gh-pages from @ Daria3025/goit-js-hw-09@45bf55bdd5ea4012e7eb320bcbe59842e647e45d 🚀

---
+++ 01-color-switcher.211223cd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");t.addEventListener("click",(function(){t.setAttribute("disabled",!0),e.removeAttribute("disabled"),d=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;r.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled",!0),clearInterval(d)})),e.setAttribute("disabled",!0);let d=null;
+//# sourceMappingURL=01-color-switcher.211223cd.js.map
